ac9dafc690b432903beafe5b72fdd438325df221
Po Linn Chia <chiapolinn@gmail.com>
2015-02-16 16:38:32 -0700

    Do some routing

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('form');
+});
+
+app.get('/success', function(req, res) {
+  res.render('success');
+});
+
+app.post('/formsubmit', function(req, res) {
+  res.redirect('success');
 });
 
 var server = app.listen(6333, function() {
